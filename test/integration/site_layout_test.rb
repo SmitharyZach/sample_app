a476888c24062f6530d603c980fb793ac1425d67
SmitharyZach <znathanielsmith@gmail.com>
2020-05-25 14:13:19 -0400

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup 
+    @user = users(:zach)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -11,5 +15,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path 
     get contact_path
     assert_select "title", full_title("Contact")
+  end
+
+  test "Users page loads with correct user" do
+    log_in_as(@user)
+    get users_path
+    assert_select "title", full_title("All users")
+    assert_select 'li'
   end
 end
