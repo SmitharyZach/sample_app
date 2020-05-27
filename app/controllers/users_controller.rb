84c74a6babb0f06dc85a13290527b0d46ef3a488
SmitharyZach <znathanielsmith@gmail.com>
2020-05-27 17:33:14 -0400

    fix host name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Thanks for signing up! Chcek your email to activate your account"
+      flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render 'new'
