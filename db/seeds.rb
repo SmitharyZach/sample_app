179094ef7da6ab53cb769defc062249f5205dbfb
SmitharyZach <znathanielsmith@gmail.com>
2020-06-02 16:28:51 -0400

    add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 
 # create a main sample user
 
-User.create!(name: "Example User", email: "example@example.com", password: "password", password_confirmation: "password", 
+User.create!(name: "Zach Smith", email: "znathanielsmith@gmail.com", password: "password", password_confirmation: "password", 
             admin: true, activated: true, activated_at: Time.zone.now)
 
 # Generate additional users
@@ -19,4 +19,13 @@ User.create!(name: "Example User", email: "example@example.com", password: "pass
     email = "example-#{n+1}@example.com"
     password = "password"
     User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
+end
+
+# Generate microposts for a subset of users.
+
+users = User.order(:created_at).take(6)
+
+50.times do 
+    content = Faker::Movies::Lebowski.quote
+    users.each { |user| user.microposts.create!(content: content) }
 end
