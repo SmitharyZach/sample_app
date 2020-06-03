c9298ad55f0e057d19b0363ba2ccfdd738d7e58c
SmitharyZach <znathanielsmith@gmail.com>
2020-06-03 17:01:16 -0400

    update env

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => 'herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 465,
     :authentication => :plain,
